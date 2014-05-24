1070ecb3a2b32c6523b1f337fa138d6342515252
skyboy <skyboy026@gmail.com>
2014-05-24 03:46:11 -0400

    Reduce JRE to 1.6, add command line switch

---
+++ src/immibis/bon/ClassCollection.java
@@ -0,0 +1,69 @@
+package immibis.bon;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.objectweb.asm.tree.ClassNode;
+
+public class ClassCollection implements Cloneable {
+	public ClassCollection(NameSet nameSet, Collection<ClassNode> classes) {
+		this.nameSet = nameSet;
+		this.classes.addAll(classes);
+	}
+	
+	private NameSet nameSet;
+	private Collection<ClassNode> classes = new ArrayList<ClassNode>();
+	private Map<String, byte[]> extraFiles = new HashMap<String, byte[]>();
+	
+	public Collection<ClassNode> getAllClasses() {
+		return classes;
+	}
+	
+	public NameSet getNameSet() {
+		return nameSet;
+	}
+	
+	@Override
+	public ClassCollection clone() {
+		try {
+			ClassCollection clone = (ClassCollection)super.clone();
+			clone.classes = new ArrayList<ClassNode>();
+			
+			for(ClassNode ocn : classes) {
+				// clone the ClassNode
+				ClassNode ncn = new ClassNode();
+				ocn.accept(ncn);
+				clone.classes.add(ncn);
+			}
+			
+			// copy map, but don't copy data
+			clone.extraFiles = new HashMap<String, byte[]>(extraFiles);
+			
+			return clone;
+			
+		} catch(CloneNotSupportedException e) {
+			throw new RuntimeException("This can't happen", e);
+		}
+	}
+	
+	public ClassCollection cloneWithNameSet(NameSet newNS) {
+		ClassCollection rv = clone();
+		rv.nameSet = newNS;
+		return rv;
+	}
+
+	public Map<String, ClassNode> getClassMap() {
+		Map<String, ClassNode> rv = new HashMap<String, ClassNode>();
+		for(ClassNode cn : classes)
+			rv.put(cn.name, cn);
+		return rv;
+	}
+
+	public Map<String, byte[]> getExtraFiles() {
+		return extraFiles;
+	}
+
+	
+}
