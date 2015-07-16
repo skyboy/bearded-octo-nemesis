0ff6e6dce0db88c0dcebed7cd1e009cedbe65ef0
skyboy <mr.wraith.06@gmail.com>
2015-07-16 19:07:54 -0400

    Add Access Transforming

--- src/immibis/bon/cui/MCPRemap.java
+++ src/immibis/bon/cui/MCPRemap.java
@@ -1,5 +1,6 @@
 package immibis.bon.cui;
 
+import immibis.bon.AccessTransformer;
 import immibis.bon.ClassCollection;
 import immibis.bon.NameSet;
 import immibis.bon.Remapper;
@@ -15,7 +16,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class MCPRemap extends CUIBase {
-	
+
 	private static class Timer {
 		private long start;
 		public Timer() {start = System.currentTimeMillis();}
@@ -26,23 +27,21 @@ public class MCPRemap extends CUIBase {
 			return rv;
 		}
 	}
-	
+
 	@Override
 	protected void run() throws Exception {
 		Timer timer = new Timer();
-		int readTime = 0, remapTime = 0, writeTime = 0;
-		
-		MappingFactory.quiet = quiet;
-		
+		int readTime = 0, remapTime = 0, transformTime = 0, writeTime = 0;
+
 		System.out.println("Loading MCP configuration");
-		
+
 		String mcVer = MappingLoader_MCP.getMCVer(mcpDir);
 		MappingFactory.registerMCPInstance(mcVer, side, mcpDir, null);
 		readTime += timer.flip();
-		
+
 		NameSet inputNS = new NameSet(fromType, side, mcVer);
 		NameSet outputNS = new NameSet(toType, side, mcVer);
-		
+
 		List<ClassCollection> refs = new ArrayList<ClassCollection>();
 		for(RefOption ro : refOptsParsed) {
 			NameSet refNS = new NameSet(ro.type, side, mcVer);
@@ -51,37 +50,43 @@ public class MCPRemap extends CUIBase {
 				System.out.println("Loading "+ro.file.getName());
 			ClassCollection refCC = ClassCollectionFactory.loadClassCollection(refNS, ro.file, null);
 			readTime += timer.flip();
-			
+
 			if(!refNS.equals(inputNS)) {
 				if (!quiet)
 					System.out.println("Remapping "+ro.file.getName()+" ("+refNS+" -> "+inputNS+")");
 				refCC = Remapper.remap(refCC, inputNS, Collections.<ClassCollection>emptyList(), null);
 				remapTime += timer.flip();
 			}
-			
+
 			refs.add(refCC);
 		}
-		
+
 		if (!quiet)
 			System.out.println("Loading "+inFile.getName());
 		ClassCollection inputCC = ClassCollectionFactory.loadClassCollection(inputNS, inFile, null);
 		readTime += timer.flip();
-		
+
+		if (!quiet)
+			System.out.println("Applying AccessTransformers");
+		inputCC = AccessTransformer.remap(inputCC, refs, null);
+		transformTime += timer.flip();
+
 		System.out.println("Remapping "+inFile.getName()+" ("+inputNS+" -> "+outputNS+")");
 		ClassCollection outputCC = Remapper.remap(inputCC, outputNS, refs, null);
 		remapTime += timer.flip();
-		
+
 		System.out.println("Writing "+outFile.getName());
 		JarWriter.write(outFile, outputCC, keepManifest, null);
 		writeTime += timer.flip();
-		
+
 		if (!quiet)
-			System.out.printf("Completed in %d ms (%dms read, %dms remap, %dms write)\n", readTime+remapTime+writeTime, readTime, remapTime, writeTime);
+			System.out.printf("Completed in %d ms (%dms read, %dms remap, %dms AT, %dms write)\n", readTime+remapTime+transformTime+writeTime,
+				readTime, remapTime, transformTime, writeTime);
 		else
-			System.out.printf("Completed in %d ms\n", readTime + remapTime + writeTime);
+			System.out.printf("Completed in %d ms\n", readTime + remapTime + transformTime + writeTime);
 	}
-	
-	
+
+
 	@Required @Option("-mcp")	public File mcpDir;
 	@Required @Option("-in")	public File inFile;
 	@Required @Option("-out")	public File outFile;
@@ -91,47 +96,52 @@ public class MCPRemap extends CUIBase {
 	          @Option("-ref")	public List<String> refOpts = new ArrayList<String>();
 	          @Option("-refn")	public List<String> refnOpts = new ArrayList<String>();
 	          @Option("-jref")	public List<String> jrefOpts = new ArrayList<String>();
+	          @Option("-at")	public List<String> atOpts = new ArrayList<String>();
 	          @Option("-q")		public boolean quiet = false;
 	          @Option("-m")		public boolean keepManifest = false;
-	
+
     private static class RefOption {
     	public NameSet.Type type;
     	public File file;
-    	
+
     	public RefOption(NameSet.Type t, File f) {
     		type = t;
     		file = f;
     	}
     }
-	          
+
 	private List<RefOption> refOptsParsed = new ArrayList<RefOption>();
-	
+
+	private List<File> atOptsParsed = new ArrayList<File>();
+
 	@Override
 	protected boolean checkOptions() throws Exception {
 		if(!super.checkOptions())
 			return false;
-		
+
+		MappingFactory.quiet = quiet;
+
 		boolean ok = true;
-		
+
 		if(!inFile.exists()) {
 			System.err.println("Input file doesn't exist: " + inFile.getAbsolutePath());
 			ok = false;
 		}
-		
+
 		if(outFile.isDirectory()) {
 			System.err.println("Output file is a directory: " + outFile.getAbsolutePath());
 			ok = false;
 		}
-		
+
 		if(!mcpDir.exists()) {
 			System.err.println("MCP directory doesn't exist: " + mcpDir.getAbsolutePath());
 			ok = false;
 		}
-		
+
 		for(String s : refOpts) {
 			refOptsParsed.add(new RefOption(fromType, new File(s)));
 		}
-		
+
 		for(String s : refnOpts) {
 			String[] p = s.split(":", 2);
 			if(p.length != 2) {
@@ -146,7 +156,7 @@ public class MCPRemap extends CUIBase {
 				}
 			}
 		}
-		
+
 		for(String s1 : jrefOpts) {
 			String[] sl;
 			if (s1.indexOf(File.pathSeparatorChar) > 0)
@@ -172,19 +182,62 @@ public class MCPRemap extends CUIBase {
 					refOptsParsed.add(new RefOption(fromType, f));
 			}
 		}
-		
+
+		for(String s1 : atOpts) {
+			String[] sl;
+			if (s1.indexOf(File.pathSeparatorChar) > 0)
+				sl = s1.split(File.pathSeparator);
+			else
+				sl = new String[] {s1};
+
+			for (String s : sl) {
+				File d = new File(s);
+				if (!d.isDirectory()) {
+					if (d.isFile()) {
+						atOptsParsed.add(d);
+					} else {
+						System.err.println("Invalid input for -at. Not a directory: " + s);
+					}
+					continue;
+				}
+				File[] l = d.listFiles(new FilenameFilter() {
+					@Override
+					public boolean accept(File dir, String name) {
+						return name.endsWith("_at.cfg") && new File(dir, name).isFile();
+					}
+				});
+				if (l == null || l.length == 0)
+					continue;
+				for (File f : l)
+					atOptsParsed.add(f);
+			}
+		}
+
 		for(RefOption ro : refOptsParsed) {
 			if(!ro.file.exists()) {
 				System.err.println("Reference file doesn't exist: " + ro.file.getAbsolutePath());
 				ok = false;
 			}
 		}
-		
+
+		for(File ro : atOptsParsed) {
+			if(!ro.exists()) {
+				System.err.println("AccessTransformer file doesn't exist: " + ro.getAbsolutePath());
+			} else {
+				try {
+					AccessTransformer.processTransformer(ro);
+				} catch (RuntimeException ex) {
+					System.err.println("AccessTransformer " + ro.getAbsolutePath() + " caused error: " + ex.getMessage());
+					ok = false;
+				}
+			}
+		}
+
 		return ok;
 	}
-	
 
-	
+
+
 	@Override
 	protected void showUsage() {
 		System.out.println("Usage:");
@@ -226,6 +279,12 @@ public class MCPRemap extends CUIBase {
 		System.out.println("       This option *is* required to be a directory, and you may specify multiple using the");
 		System.out.println("       path separator character (; on windows, : on *nix) on your OS, or multiple options.");
 		System.out.println("");
+		System.out.println("  -at <AT dir(s)>");
+		System.out.println("       Specifies the path to a directory which contains _at.cfg files the input code depends on.");
+		System.out.println("       It is assumed that all ATs in the directory are using the same mapping as the input jar.");
+		System.out.println("       You can also pass individual files to this option. You may specify multiple using the");
+		System.out.println("       path separator character (; on windows, : on *nix) on your OS, or multiple options.");
+		System.out.println("");
 		System.out.println("  -m");
 		System.out.println("       Will ensure the manifest file (when remapping a jar) will be kept in the generated output jar.");
 		System.out.println("");
@@ -252,7 +311,7 @@ public class MCPRemap extends CUIBase {
 		System.out.println("A reference file could be the output of a previous command.");
 		System.out.println("");
 	}
-	
+
 	public static void main(String[] args) throws Exception {
 		new MCPRemap().run(args);
 	}
--- src/immibis/bon/mcp/MappingLoader_MCP.java
+++ src/immibis/bon/mcp/MappingLoader_MCP.java
@@ -17,17 +17,17 @@ import java.util.Scanner;
 import java.util.Set;
 
 public class MappingLoader_MCP {
-	
+
 	public static class CantLoadMCPMappingException extends Exception {
 		private static final long serialVersionUID = 1;
-		
+
 		public CantLoadMCPMappingException(String reason) {
 			super(reason);
 		}
 	}
-	
-	
-	
+
+
+
 	@SuppressWarnings("unused")
 	private final Side side;
 	@SuppressWarnings("unused")
@@ -35,26 +35,26 @@ public class MappingLoader_MCP {
 	private final File mcpDir;
 	private final int[] sideNumbers;
 	private final File srgFile, excFile;
-	
+
 	// forward: obf -> searge -> mcp
 	// reverse: mcp -> searge -> obf
 	private Mapping forwardSRG, reverseSRG, forwardCSV, reverseCSV;
-	
-	
+
+
 	private Map<String, Set<String>> srgMethodDescriptors = new HashMap<String, Set<String>>(); // SRG name -> SRG descriptors
 	private Map<String, Set<String>> srgMethodOwners = new HashMap<String, Set<String>>(); // SRG name -> SRG owners
 	private Map<String, Set<String>> srgFieldOwners = new HashMap<String, Set<String>>(); // SRG name -> SRG owners
-	
+
 	private ExcFile excFileData;
-	
+
 	public MappingLoader_MCP(String mcVer, Side side, File mcpDir, IProgressListener progress) throws IOException, CantLoadMCPMappingException {
 		this.mcVer = mcVer;
 		this.mcpDir = mcpDir;
 		this.side = side;
-		
+
 		switch(side) {
 		case UNIVERSAL:
-			sideNumbers = new int[] {2, 0};
+			sideNumbers = new int[] {2, 1, 0};
 			if(new File(mcpDir, "conf/packaged.srg").exists()) {
 				srgFile = new File(mcpDir, "conf/packaged.srg");
 				excFile = new File(mcpDir, "conf/packaged.exc");
@@ -63,42 +63,42 @@ public class MappingLoader_MCP {
 				excFile = new File(mcpDir, "conf/joined.exc");
 			}
 			break;
-			
+
 		case CLIENT:
 			sideNumbers = new int[] {0};
 			srgFile = new File(mcpDir, "conf/client.srg");
-			
+
 			if(new File(mcpDir, "conf/joined.exc").exists())
 				excFile = new File(mcpDir, "conf/joined.exc");
 			else
 				excFile = new File(mcpDir, "conf/client.exc");
-			
+
 			break;
-			
+
 		case SERVER:
 			sideNumbers = new int[] {1};
 			srgFile = new File(mcpDir, "conf/server.srg");
-			
+
 			if(new File(mcpDir, "conf/joined.exc").exists())
 				excFile = new File(mcpDir, "conf/joined.exc");
 			else
 				excFile = new File(mcpDir, "conf/server.exc");
-			
+
 			break;
-			
+
 		default: throw new AssertionError("side is "+side);
 		}
-		
+
 		NameSet obfNS = new NameSet(NameSet.Type.OBF, side, mcVer);
 		NameSet srgNS = new NameSet(NameSet.Type.SRG, side, mcVer);
 		NameSet mcpNS = new NameSet(NameSet.Type.MCP, side, mcVer);
-		
+
 		forwardSRG = new Mapping(obfNS, srgNS);
 		reverseSRG = new Mapping(srgNS, obfNS);
-		
+
 		forwardCSV = new Mapping(srgNS, mcpNS);
 		reverseCSV = new Mapping(mcpNS, srgNS);
-		
+
 		if(progress != null) progress.setMax(3);
 		if(progress != null) progress.set(0);
 		loadEXCFile();
@@ -107,76 +107,76 @@ public class MappingLoader_MCP {
 		if(progress != null) progress.set(2);
 		loadCSVMapping();
 	}
-	
+
 	private void loadEXCFile() throws IOException {
 		excFileData = new ExcFile(excFile);
 	}
-	
-	
+
+
 
 	private void loadSRGMapping() throws IOException, CantLoadMCPMappingException {
 		SrgFile srg = new SrgFile(srgFile, false);
-		
+
 		forwardSRG.setDefaultPackage("net/minecraft/src/");
 		reverseSRG.addPrefix("net/minecraft/src/", "");
-		
+
 		for(Map.Entry<String, String> entry : srg.classes.entrySet()) {
 			String obfClass = entry.getKey();
 			String srgClass = entry.getValue();
-			
+
 			forwardSRG.setClass(obfClass, srgClass);
 			reverseSRG.setClass(srgClass, obfClass);
 		}
-		
+
 		for(Map.Entry<String, String> entry : srg.fields.entrySet()) {
 			String obfOwnerAndName = entry.getKey();
 			String srgName = entry.getValue();
-			
+
 			String obfOwner = obfOwnerAndName.substring(0, obfOwnerAndName.lastIndexOf('/'));
 			String obfName = obfOwnerAndName.substring(obfOwnerAndName.lastIndexOf('/') + 1);
-			
+
 			String srgOwner = srg.classes.get(obfOwner);
-			
+
 			// Enum values don't use the CSV and don't start with field_
 			if(srgName.startsWith("field_")) {
 				if(srgFieldOwners.containsKey(srgName))
 					System.out.println("SRG field "+srgName+" appears in multiple classes (at least "+srgFieldOwners.get(srgName)+" and "+srgOwner+")");
-				
+
 				Set<String> owners = srgFieldOwners.get(srgName);
 				if(owners == null)
 					srgFieldOwners.put(srgName, owners = new HashSet<String>());
 				owners.add(srgOwner);
 			}
-			
+
 			forwardSRG.setField(obfOwner, obfName, srgName);
 			reverseSRG.setField(srgOwner, srgName, obfName);
 		}
-		
+
 		for(Map.Entry<String, String> entry : srg.methods.entrySet()) {
 			String obfOwnerNameAndDesc = entry.getKey();
 			String srgName = entry.getValue();
-			
+
 			String obfOwnerAndName = obfOwnerNameAndDesc.substring(0, obfOwnerNameAndDesc.indexOf('('));
 			String obfOwner = obfOwnerAndName.substring(0, obfOwnerAndName.lastIndexOf('/'));
 			String obfName = obfOwnerAndName.substring(obfOwnerAndName.lastIndexOf('/') + 1);
 			String obfDesc = obfOwnerNameAndDesc.substring(obfOwnerNameAndDesc.indexOf('('));
-			
+
 			String srgDesc = forwardSRG.mapMethodDescriptor(obfDesc);
 			String srgOwner = srg.classes.get(obfOwner);
-			
+
 			Set<String> srgMethodDescriptorsThis = srgMethodDescriptors.get(srgName);
 			if(srgMethodDescriptorsThis == null)
 				srgMethodDescriptors.put(srgName, srgMethodDescriptorsThis = new HashSet<String>());
 			srgMethodDescriptorsThis.add(srgDesc);
-			
+
 			Set<String> srgMethodOwnersThis = srgMethodOwners.get(srgName);
 			if(srgMethodOwnersThis == null)
 				srgMethodOwners.put(srgName, srgMethodOwnersThis = new HashSet<String>());
 			srgMethodOwnersThis.add(srgOwner);
-			
+
 			forwardSRG.setMethod(obfOwner, obfName, obfDesc, srgName);
 			reverseSRG.setMethod(srgOwner, srgName, srgDesc, obfName);
-			
+
 			String[] srgExceptions = excFileData.getExceptionClasses(srgOwner, srgName, srgDesc);
 			if(srgExceptions.length > 0)
 			{
@@ -187,15 +187,15 @@ public class MappingLoader_MCP {
 			}
 		}
 	}
-	
+
 	private void loadCSVMapping() throws IOException, CantLoadMCPMappingException  {
 		Map<String, String> fieldNames = CsvFile.read(new File(mcpDir, "conf/fields.csv"), sideNumbers);
 		Map<String, String> methodNames = CsvFile.read(new File(mcpDir, "conf/methods.csv"), sideNumbers);
-		
+
 		for(Map.Entry<String, String> entry : fieldNames.entrySet()) {
 			String srgName = entry.getKey();
 			String mcpName = entry.getValue();
-			
+
 			if(srgFieldOwners.get(srgName) == null) {
 				if (!MappingFactory.quiet)
 					System.out.println("Field exists in CSV but not in SRG: "+srgName+" (CSV name: "+mcpName+")");
@@ -203,17 +203,17 @@ public class MappingLoader_MCP {
 			} else {
 				for(String srgOwner : srgFieldOwners.get(srgName)) {
 					String mcpOwner = srgOwner;
-					
+
 					forwardCSV.setField(srgOwner, srgName, mcpName);
 					reverseCSV.setField(mcpOwner, mcpName, srgName);
 				}
 			}
 		}
-		
+
 		for(Map.Entry<String, String> entry : methodNames.entrySet()) {
 			String srgName = entry.getKey();
 			String mcpName = entry.getValue();
-			
+
 			if(srgMethodOwners.get(srgName) == null) {
 				if (!MappingFactory.quiet)
 					System.out.println("Method exists in CSV but not in SRG: "+srgName+" (CSV name: "+mcpName+")");
@@ -223,7 +223,7 @@ public class MappingLoader_MCP {
 					for (String srgDesc : srgMethodDescriptors.get(srgName)) {
 						String mcpOwner = srgOwner;
 						String mcpDesc = srgDesc;
-					
+
 						forwardCSV.setMethod(srgOwner, srgName, srgDesc, mcpName);
 						reverseCSV.setMethod(mcpOwner, mcpName, mcpDesc, srgName);
 					}
